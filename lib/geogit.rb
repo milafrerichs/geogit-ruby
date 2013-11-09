b8ae10d9ee553563063c11dabf678178f3952db7
Scooter Wadsworth <scooterwadsworth@gmail.com>
2013-11-08 23:11:15 -0700

    Add GeoGit::Instance class

--- lib/geogit.rb
+++ lib/geogit.rb
@@ -6,6 +6,9 @@ if defined? JRUBY_VERSION
   Dir[File.join(File.expand_path('..', __FILE__), '..', 'geogit-libs', '*.jar')].each {|jar| require jar}
 
   require 'geogit/geogit'
+  require 'geogit/commands/generic_command'
+  require 'geogit/commands/log'
 else
   abort "JRuby is required for this application (http://jruby.org)"
 end
+
--- lib/geogit/geogit.rb
+++ lib/geogit/geogit.rb
@@ -1,30 +1,54 @@
-java_import org.geogit.api.GlobalInjectorBuilder
 java_import org.geogit.storage.bdbje.JEStorageModule
 java_import org.geogit.storage.neo4j.Neo4JModule
 java_import org.geogit.di.GeogitModule
 java_import org.geogit.api.GeoGIT
 java_import org.geogit.api.DefaultPlatform
+java_import org.geogit.api.GeogitTransaction
+java_import com.google.inject.Guice
+java_import com.google.inject.util.Modules
 
 module GeoGit
-  class << self
-    def setup_geogit
-      geogit_module = GeogitModule.new
-      je_storage_module = JEStorageModule.new
-      neo4j_module = Neo4JModule.new
-      GlobalInjectorBuilder.builder.build_with_overrides je_storage_module, neo4j_module
+  class Instance
+    attr_accessor :repo_path, :instance
+
+    def initialize(repo_path)
+      @repo_path = File.expand_path repo_path
+      @injector = get_injector
+      @instance = get_instance @repo_path
+    end
+    
+    def get_command_locator(transaction_id = nil)
+      unless transaction_id.nil?
+        GeoGitTranaction.new(
+          @instance.get_command_locator,
+          @instance.get_repository,
+          transaction_id
+        )
+      else
+        @instance.get_command_locator
+      end
     end
 
+    protected
+    def get_injector
+      je_storage_module = JEStorageModule.new
+      neo4j_module = Neo4JModule.new
+      Guice.create_injector Modules.override(GeogitModule.new).with(je_storage_module, neo4j_module)
+    end
+
+    protected
     def get_instance(path)
       begin
         platform = DefaultPlatform.new
         platform.set_working_dir java.io.File.new(path)
-        geogit = GeoGIT.new(setup_geogit, platform.pwd)
+        geogit = GeoGIT.new(get_injector, platform.pwd)
         geogit.get_or_create_repository
         geogit
       rescue => ex
         puts ex
       end
     end
+
   end
 end
 
