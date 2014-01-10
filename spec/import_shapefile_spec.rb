8e86b7710ae5cdc211d8c7080f6a79f3775dca8e
Scooter Wadsworth <scooterwadsworth@gmail.com>
2014-01-10 00:04:23 -0700

    Start adding tests

---
+++ spec/import_shapefile_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path 'spec/spec_helper'
+require File.expand_path 'lib/geogit'
+require 'fileutils'
+
+describe GeoGit::Command::ImportShapefile do
+
+  before(:all) do
+    repo_path = File.expand_path '~/geogit/test_repo'
+    FileUtils.mkdir_p repo_path
+    GeoGit::Command::Init.new(repo_path).run
+  end
+
+  after(:all) do
+    FileUtils.rm_rf File.expand_path('~/geogit/test_repo')
+  end
+
+  it "should import 49 states" do
+    repo_path = File.expand_path '~/geogit/test_repo'
+    shp_path = File.expand_path 'spec/data/shapefile/states.shp'
+    GeoGit::Command::ImportShapefile.new(repo_path, shp_path).run
+    GeoGit::Command::Tree.new(repo_path, 'states').run.size.should eq 49
+  end
+
+end
+
---
+++ spec/init_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path 'spec/spec_helper'
+require File.expand_path 'lib/geogit'
+require 'fileutils'
+
+describe GeoGit::Command::Init do
+
+  before(:all) do
+    FileUtils.mkdir_p File.expand_path('~/geogit/test_repo')
+  end
+
+  after(:all) do
+    FileUtils.rm_rf File.expand_path('~/geogit/test_repo')
+  end
+
+  it "should create empty repo" do
+    repo_path = File.expand_path('~/geogit/test_repo')
+    GeoGit::Command::Init.new(repo_path).run
+    Dir.exist?(File.join(repo_path, '.geogit')).should be_true
+  end
+
+end
+
