ad9b7415d9e0cbe08861c784e170ef23add26d3b
Scooter Wadsworth <scooterwadsworth@gmail.com>
2014-01-11 00:25:37 -0700

    Add geojson import spec

---
+++ spec/import_geojson_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path 'spec/spec_helper'
+require File.expand_path 'lib/geogit'
+require 'fileutils'
+
+describe GeoGit::Command::ImportGeoJSON do
+
+  before :all do
+    repo_path = File.expand_path '~/geogit/test_repo'
+    FileUtils.mkdir_p repo_path
+    GeoGit::Command::Init.new(repo_path).run
+  end
+
+  after :all do
+    FileUtils.rm_rf File.expand_path('~/geogit/test_repo')
+  end
+
+  it "should import hurricanes" do
+    repo_path = File.expand_path '~/geogit/test_repo'
+    geojson_path = File.expand_path 'spec/data/geojson/fl_2004_hurricanes.geojson'
+    GeoGit::Command::ImportGeoJSON.new(repo_path, geojson_path).run
+    GeoGit::Command::Tree.new(repo_path, 'feature').run.size.should eq 652
+  end
+
+end
+
